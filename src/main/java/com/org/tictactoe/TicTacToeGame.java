6a913e5b3422684937e0961cb64202806b256ea4
maqsood <maqsoodhamd37@gmail.com>
2020-10-08 10:15:29 +0530

    [Maqsood].refactor: added documentation for setUpBoard method

--- src/main/java/com/org/tictactoe/TicTacToeGame.java
+++ src/main/java/com/org/tictactoe/TicTacToeGame.java
@@ -6,11 +6,16 @@ import org.apache.logging.log4j.Logger;
 public class TicTacToeGame {
 	static final Logger logger = LogManager.getLogger(TicTacToeGame.class);
 	static char[] board;
-	public static void setUpBoard() {
+	/**
+	 * uc1
+	 * @return
+	 */
+	public static char[] setUpBoard() {
 		board=new char[10];
 		for (int i = 0; i < board.length-1; i++) {
 			board[i] = ' ';
 		}
+		return board;
 	}
 
 	public static void main(String[] args) {
