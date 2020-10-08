dd9a91c684abdcdd602ae76f7a0afaffe1457329
maqsood <maqsoodhamd37@gmail.com>
2020-10-08 10:06:08 +0530

    [Maqsood].refactor: made board as a static variable

--- src/main/java/com/org/tictactoe/TicTacToeGame.java
+++ src/main/java/com/org/tictactoe/TicTacToeGame.java
@@ -5,16 +5,15 @@ import org.apache.logging.log4j.Logger;
 
 public class TicTacToeGame {
 	static final Logger logger = LogManager.getLogger(TicTacToeGame.class);
-
-	public static char[] setUpBoard() {
-		char[] board=new char[10];
+	static char[] board;
+	public static void setUpBoard() {
+		board=new char[10];
 		for (int i = 0; i < board.length-1; i++) {
 			board[i] = ' ';
 		}
-		return board;
 	}
 
 	public static void main(String[] args) {
-		char[] board = setUpBoard();
+		setUpBoard();		
 	}
 }
