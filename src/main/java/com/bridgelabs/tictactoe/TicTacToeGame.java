5af6b5ba6bc508d96a6b44f349d6082e905ad282
maqsood <maqsoodhamd37@gmail.com>
2020-10-08 10:43:49 +0530

    [Maqsood].update: added feature to choose and assign player letter and computer letter

--- src/main/java/com/bridgelabs/tictactoe/TicTacToeGame.java
+++ src/main/java/com/bridgelabs/tictactoe/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.bridgelabs.tictactoe;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,8 +18,31 @@ public class TicTacToeGame {
 		}
 		return board;
 	}
-
+	
+	public static char chooseLetter(Scanner sc) {
+		logger.info("Enter the letter choice ('X' or 'O'): ");
+		char playerChoice=sc.nextLine().charAt(0);
+		if(playerChoice=='X') {
+		logger.info("Player Letter is 'X'. Computer Letter is 'O'.");
+		}
+		else {
+			logger.info("Player Letter is 'O'. Computer Letter is 'X'.");
+		}
+		return playerChoice;
+	}
 	public static void main(String[] args) {
-		char[] board= setUpBoard();		
+		Scanner sc=new Scanner(System.in);
+		char[] board= setUpBoard();
+		char playerLetter, computerLetter;
+		char playerChoice=chooseLetter(sc);
+		if(playerChoice=='X') {
+			playerLetter='X';
+			computerLetter='O';
+		}
+		else {
+			playerLetter='O';
+			computerLetter='X';
+		}
+		
 	}
 }
